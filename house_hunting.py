5b2fa92c0031599ae005a731ff0faf7f451edde9
Arthurstuart <Arthur.stuart10@gmail.com>
2021-02-16 02:21:48 -0500

    working still. will revist

--- house_hunting.py
+++ house_hunting.py
@@ -1,1 +1,17 @@
 # Write your code here
+total_cost = input("The total cost of your home?")
+annual_salary = input("What's your annual salary?")
+numofMonths = 0
+fundsInSavings = input("Ammount of funds in your savings? [in decimals]")
+current_savings = 0
+portion_down_payment = 0.25
+portion_saved = 10% 
+
+def buyingPower (annual_salary, current_savings, fundsInSavings)
+r = 0.04
+(annual_salary/12 )
+
+interest = ((fundsInSavings * r) / 12)
+while houseApproval == False
+if total_cost == numofMonths
+# We're working up to being able to determine how to determine how many months would be needed to by the house
